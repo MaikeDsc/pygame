72f4394d4c9b2301fdd028b4d38181f5c2accea7
MaikeDsc <maquiavelcampos@gmail.com>
2025-09-09 16:20:49 -0400

    Adicionado arquivos do vilão jogo sussurus da selva

---
+++ sussurus/principal.py
@@ -0,0 +1,109 @@
+import pygame
+from vilao import Vilao
+from vilao import Projeteis
+
+import pygame
+
+pygame.init()
+
+largura = 900
+altura = int(largura * 0.7)   
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption('vilão')
+
+#taxas de quadro (frame rate)
+relogio = pygame.time.Clock()
+fps = 60
+
+
+#carregando u a imagem para o bg
+imagem_fundo = pygame.image.load('sussurus/imagens/fundo/selva2.png')
+imagem_fundo = pygame.transform.scale(imagem_fundo, (largura,altura))
+
+
+#estados do curupira
+estado_obsoleto = 0
+estado_atacando = 1
+estado_atual= estado_obsoleto
+#setando os tempo dos ataques 
+tempo_obsoleto = 3000    #em milisegundos pq o relogio só conta em milisegundos
+tempo_lancando = 2400
+
+time_inicio_estado = pygame.time.get_ticks()
+
+
+#--------------------------------------------------------------------------------
+
+#função para desenhar o fundo
+def desenhar_bg():
+    tela.blit(imagem_fundo, (0,0))
+
+  
+#--------------------------------------------------------------------------------
+
+#define a instancia player 
+curupira = Vilao('curupira', 700, 475, 1.7, tela)
+
+bola1 = Projeteis('curupira',900, 300, 2, tela )
+bola2 = Projeteis('curupira',900, 500, 2, tela )
+bola3 = Projeteis('curupira',900, 100, 2, tela )
+
+
+
+
+
+loop = True
+while loop:
+
+    relogio.tick(fps)
+
+    desenhar_bg()                       #backgrownd
+
+    curupira.atualizar_animacao()         #atualiza o frame antes de desenhar
+    bola1.atualizar_animacao()
+    bola2.atualizar_animacao()
+    #bola3.atualizar_animacao()
+    
+    curupira.draw()                       #desenhar as imagem com o metodo draw
+   
+    
+
+    for event in pygame.event.get():
+
+        #eventos de usuario
+        if event.type == pygame.QUIT:
+            loop = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                loop = False
+
+
+    time_atual = pygame.time.get_ticks()
+
+    if estado_atual == estado_obsoleto:
+        if time_atual - time_inicio_estado >= tempo_obsoleto:
+            estado_atual = estado_atacando
+            curupira.atualizar_acoes(1)
+            time_inicio_estado = time_atual
+
+    if estado_atual == estado_atacando:
+        bola1.draw()
+        bola1.movimento()
+        bola2.draw()
+        bola2.movimento()
+        #bola3.draw()
+        #bola3.movimento()
+        if time_atual - time_inicio_estado >= tempo_lancando:
+            estado_atual = estado_obsoleto
+            
+            curupira.atualizar_acoes(0)
+            time_inicio_estado = time_atual
+
+            #aqui tem q redefinir a posição da bola p poder ela aparecer novamente
+            bola1.rect.x = 900
+            bola2.rect.x = 900
+            #bola3.rect.x = 900
+    
+    pygame.display.update()
+pygame.quit()
---
+++ sussurus/vilao.py
@@ -0,0 +1,109 @@
+import pygame
+
+
+class Vilao(pygame.sprite.Sprite):
+    def __init__(self, nome_pasta, x, y, escala, tela):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.atualizar_time = pygame.time.get_ticks()
+        self.nome_pasta = nome_pasta
+        self.lista_animacoes = []
+        self.indice_frame = 0
+        self.acao = 0
+        self.tela = tela
+
+        
+        #sprites obsoleto
+        lista_temporaria = []
+        for c in range (3):
+            imagem = pygame.image.load(f'sussurus/imagens/{self.nome_pasta}/obsoleto/{c}.png')
+            imagem = pygame.transform.scale(imagem, (int(imagem.get_width() * escala), int(imagem.get_height() * escala)) )
+            lista_temporaria.append(imagem)
+        self.lista_animacoes.append(lista_temporaria)
+
+        #sprites dele lancando magia 
+        lista_temporaria = []
+        for c in range (4):
+            imagem = pygame.image.load(f'sussurus/imagens/{self.nome_pasta}/lancar_magia/{c}.png')
+            imagem = pygame.transform.scale(imagem, (int(imagem.get_width() * escala), int(imagem.get_height() * escala)) )
+            lista_temporaria.append(imagem)
+        self.lista_animacoes.append(lista_temporaria)
+
+        self.img = self.lista_animacoes[self.acao][self.indice_frame]
+        self.rect = self.img.get_rect()
+        self.rect.center = (x, y)
+
+#--------------------------------------
+
+    def atualizar_animacao(self):
+        #atualizando animação
+        INTERVALO_ANIMACAO = 200                        # é o cooldown de uma animaçaõ para outra, é uma constante
+        #atualizando o frame independente da frame tual 
+        self.img = self.lista_animacoes[self.acao][self.indice_frame]
+
+        #vendo o horario atual novamente para saber quanto tempo passou desde a ultima checagem
+        if pygame.time.get_ticks() - self.atualizar_time > INTERVALO_ANIMACAO:              #se o tempo for maior, passar para o rpoximo quadro
+            self.atualizar_time = pygame.time.get_ticks()
+            self.indice_frame += 1
+
+        #se as animações acabaram, entao renicie do começo 
+        if self.indice_frame >= len(self.lista_animacoes[self.acao]):
+            self.indice_frame = 0
+
+
+    def atualizar_acoes(self, nova_acao):
+        #checar se a vova ação é diferente da anterior
+        if nova_acao != self.acao:
+            self.acao = nova_acao                                         #uma nova ação vai ser definida 
+            self.indice_frame = 0                                           #atualiza para o primeiro frame de volta, para ficar mais fluido
+            self.atualizar_time = pygame.time.get_ticks()                   #atualiza o relogio de transição de frames tbm
+    
+    def draw(self):
+        self.tela.blit(self.img ,self.rect)
+
+
+
+#============================================================================================================
+
+class Projeteis(pygame.sprite.Sprite):
+    def __init__(self, nome_pasta, x, y, escala, tela):
+        pygame.sprite.Sprite.__init__(self)
+        self.atualizar_time = pygame.time.get_ticks()
+        self.nome_pasta = nome_pasta
+        self.lista_animacoes = []
+        self.indice_frame = 0
+        self.acao = 0
+        self.tela = tela
+
+        for c in range (3):
+            imagem = pygame.image.load(f'sussurus/imagens/{self.nome_pasta}/bola_de_fogo/{c}.png')
+            imagem = pygame.transform.scale(imagem, (int(imagem.get_width() * escala), int(imagem.get_height() * escala)) )
+            self.lista_animacoes.append(imagem)
+
+        self.img = self.lista_animacoes[self.indice_frame]
+        self.rect = self.img.get_rect()
+        self.rect.center = (x, y)
+
+    def atualizar_animacao(self):
+            #atualizando animação
+            INTERVALO_ANIMACAO = 200                        # é o cooldown de uma animaçaõ para outra, é uma constante
+            #atualizando o frame independente da frame tual 
+            self.img = self.lista_animacoes[self.indice_frame]
+
+            #vendo o horario atual novamente para saber quanto tempo passou desde a ultima checagem
+            if pygame.time.get_ticks() - self.atualizar_time > INTERVALO_ANIMACAO:              #se o tempo for maior, passar para o rpoximo quadro
+                self.atualizar_time = pygame.time.get_ticks()
+                self.indice_frame += 1
+
+            #se as animações acabaram, entao renicie do começo 
+            if self.indice_frame >= len(self.lista_animacoes):
+                self.indice_frame = 0
+    
+    def movimento(self):
+        self.rect.x -= 10
+
+    def draw(self):
+        self.tela.blit(self.img ,self.rect)
+
+
+        
